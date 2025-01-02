0510e7ef51fd0ee260f28edd4e44b8e1ebe36d05
Luna Skye <sepshuncontact@gmail.com>
2025-01-01 22:48:45 -0500

    refactor(utils): move GET method to utils package

--- utils/main.go
+++ utils/main.go
@@ -1,11 +1,18 @@
 package utils
 
 import (
+	"io"
+	"log"
 	"strings"
+	"net/http"
+	"encoding/json"
 
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
+
+	"github.com/gofiber/fiber/v2"
 )
+
 
 // Converts a snake_case string to a PascalCase string
 func SnakeToPascalCase(snakeCase string) string {
@@ -14,4 +21,21 @@ func SnakeToPascalCase(snakeCase string) string {
     words[i] = cases.Title(language.English).String(word)
   }
   return strings.Join(words, "")
+}
+
+
+// Make GET request to a URL and return the resulting JSON
+func GET(url string) fiber.Map {
+  resp, err := http.Get(url)
+  if err != nil { log.Fatalln(err) }
+  defer resp.Body.Close()
+
+  body, err := io.ReadAll(resp.Body)
+  if err != nil { log.Fatalln(err) }
+
+  data := fiber.Map{}
+  err = json.Unmarshal(body, &data)
+  if err != nil { log.Fatalln(err) }
+
+  return data
 }
