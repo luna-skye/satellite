4771cdbbd2922e33ed45958c1e9e24b6276fb0dc
Luna Skye <sepshuncontact@gmail.com>
2025-01-01 22:59:08 -0500

    fix(services): missing utils import

--- server/services/main.go
+++ server/services/main.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"luna-skye/satellite/utils"
 	"luna-skye/satellite/server/database"
 )
 
@@ -10,12 +11,12 @@ type GetResults struct {}
 
 func (s *GetResults) Radarr(service database.Service) fiber.Map {
   return fiber.Map{
-    "status": GET("http://192.168.1.238:7878/api/v3/system/status?apiKey=29fb7c6ffede430b905db2461b315c54"),
+    "status": utils.GET("http://192.168.1.238:7878/api/v3/system/status?apiKey=29fb7c6ffede430b905db2461b315c54"),
   }
 }
 
 func (s *GetResults) Sonarr(service database.Service) fiber.Map {
   return fiber.Map{
-    "status": GET(""),
+    "status": utils.GET(""),
   }
 }
